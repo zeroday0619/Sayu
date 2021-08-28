df40afd68817f719fc5665483103e69511265585
Euiseo Cha <zeroday0619_dev@outlook.com>
2021-08-28 23:04:06 +0900

    feat: 프로젝트 구조 형성 및 eslint 적용

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+// .eslintrc.js
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    extends: [
+      "plugin:@typescript-eslint/eslint-recommended"
+    ]
+};
